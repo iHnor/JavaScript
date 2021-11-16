62b25ba3000c698ba646a8c82367f03991e5c656
Ihor <igormykhno@gmail.com>
2021-11-16 09:54:01 +0200

    add function for creating title and check box

--- TodoList/script.js
+++ TodoList/script.js
@@ -1,6 +1,6 @@
 let tasksList = [
-    { title: 'Описать массив задач в JavaScript', description: "Some descript", deadline: '2021-11-21', done: false },
-    { title: 'Создать базовый макет страницы для вывода задач', description: "Some descript", deadline: '2021-11-21', done: false },
+    { title: 'Описать массив задач в JavaScript', description: "Some descript", deadline: '2021-11-21', done: true },
+    { title: 'Создать базовый макет страницы для вывода задач', deadline: '2021-11-21', done: false },
 ];
 
 let tasksOnPage = document.getElementById('tasks-On-Page')
@@ -8,18 +8,11 @@ let tasksOnPage = document.getElementById('tasks-On-Page')
 function createTask({ title, description, deadline, done }) {
     let taskDiv = document.createElement("div");
     taskDiv.className = "task";
-    taskDiv.appendChild(deadlineOfTask(deadline));
-    taskDiv.appendChild(baseDiv(title, done));
-    taskDiv.appendChild(descOfTask(description));
+    taskDiv.appendChild(createDeadline(deadline));
+    taskDiv.appendChild(createBaseDiv(title, done));
+    taskDiv.appendChild(createDescription(description));
 
     return taskDiv;
-}
-
-function createDescription(desc){
-    let descOfTask = document.createElement("p");
-    descOfTask.innerHTML = desc;
-
-    return descOfTask;
 }
 
 function createDeadline(deadline) {
@@ -29,19 +22,36 @@ function createDeadline(deadline) {
 }
 
 function createBaseDiv(title, done) {
-    let doneOfTask = document.createElement("input");
-    doneOfTask.type = 'checkbox';
+    let baseDiv = document.createElement("div");
+    baseDiv.id = "base";
+    baseDiv.appendChild(createCheckBox(done));
+    baseDiv.appendChild(createTitle(title));
 
+    return baseDiv;
+}
+
+function createTitle(title){
     let titleOfTask = document.createElement("h2");
     titleOfTask.innerHTML = title;
 
-    let baseDiv = document.createElement("div");
-    baseDiv.id = "base";
+    return titleOfTask;
+}
 
-    baseDiv.appendChild(doneOfTask);
-    baseDiv.appendChild(titleOfTask);
+function createCheckBox(done) {
+    let doneOfTask = document.createElement("input");
+    doneOfTask.type = 'checkbox';
+    if (done)
+        doneOfTask.checked = 'checked';
+    
+    return doneOfTask;
+}
 
-    return baseDiv;
+function createDescription(desc) {
+    let descOfTask = document.createElement("p");
+    if (desc !== undefined)
+        descOfTask.innerHTML = desc;
+
+    return descOfTask;
 }
 
 tasksList.forEach(t => {
